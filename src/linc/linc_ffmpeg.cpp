a47e5e3ac9f3f18ea1116cc8c5dc50da73d78c0d
Dmitry <kasimowsky@gmail.com>
2019-03-12 00:15:06 +1000

    Update linc_ffmpeg.cpp
    # no ifdefs in cpp

--- src/linc/linc_ffmpeg.cpp
+++ src/linc/linc_ffmpeg.cpp
@@ -1,11 +1,7 @@
-#ifndef _LINC_FFMPEG_CPP_
-    #define _LINC_FFMPEG_CPP_
-    
-    #include <hxcpp.h>
+#include <hxcpp.h>
 
-    #include "./linc_ffmpeg.h"
+#include "./linc_ffmpeg.h"
 
-    namespace linc {
-        namespace ffmpeg {}
-    }
-#endif
+namespace linc {
+    namespace ffmpeg {}
+}
